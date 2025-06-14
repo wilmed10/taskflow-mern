7677139f494ab19e9b303be9c20d4dfcc29c7bf0
Wilson Medina <wilsonmedina8d@gmail.com>
2025-06-13 22:40:57 -0500

    feat: config cors to allow com between front and back

--- taskflow-backend/src/server.ts
+++ taskflow-backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
+import { corsConfig } from './config/cors'
 
 dotenv.config()
 connectDB()
 
 const app = express()
+app.use(cors(corsConfig))
 
 app.use(express.json())
 
