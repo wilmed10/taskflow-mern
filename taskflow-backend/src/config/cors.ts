02b5199a5fba89db25d6fc0cceb755699c9cb394
Wilson Medina <wilsonmedina8d@gmail.com>
2025-06-26 22:49:43 -0500

    feat: add CORS support and improve project fetching in DashboardView

--- taskflow-backend/src/config/cors.ts
+++ taskflow-backend/src/config/cors.ts
@@ -1,12 +1,16 @@
-import { CorsOptions } from "cors";
+import { CorsOptions } from "cors"
+import dotenv from 'dotenv';
+dotenv.config();
+
+const whitelist = [process.env.FRONTEND_URL];
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
-        if(whitelist.includes(origin)) {
-            callback(null, true)
+        if (!origin || whitelist.includes(origin)) {
+            callback(null, true);
         } else {
             callback(new Error('Error de CORS'))
         }
-    }
-}
+    },
+    credentials: true
+};
--- taskflow-frontend/src/views/DashboardView.tsx
+++ taskflow-frontend/src/views/DashboardView.tsx
@@ -1,6 +1,16 @@
+import { getProjects } from "@/api/ProjectAPI";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
 export default function DashboardView() {
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['projects'],
+    queryFn: getProjects
+  })
+
+  if(isLoading) return 'Cargando...'
+  console.log(data)
 
   return (
     <>
