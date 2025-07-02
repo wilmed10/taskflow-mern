cdaf4e6f73aa93086f13bc4ea0e56eba7aede540
Wilson Medina <wilsonmedina8d@gmail.com>
2025-07-01 22:31:52 -0500

    feat: mejorar la visualización de proyectos en DashboardView con detalles y opciones de menú

--- taskflow-frontend/src/views/DashboardView.tsx
+++ taskflow-frontend/src/views/DashboardView.tsx
@@ -1,3 +1,6 @@
+import { Fragment } from 'react'
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 import { getProjects } from "@/api/ProjectAPI";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
@@ -10,7 +13,7 @@ export default function DashboardView() {
   })
 
   if(isLoading) return 'Cargando...'
-  console.log(data)
+  //console.log(data)
 
   if (data) return (
     <>
@@ -25,7 +28,63 @@ export default function DashboardView() {
       </nav>
 
       {data.length ? (
-        <p>Si hay proyectos</p>
+        <ul role="list" className="divide-y divide-gray-100 border border-gray-100 mt-10 bg-white shadow-lg">
+        {data.map((project) => (
+          <li key={project._id} className="flex justify-between gap-x-6 px-5 py-10">
+              <div className="flex min-w-0 gap-x-4">
+                  <div className="min-w-0 flex-auto space-y-2">
+                      <Link to={``}
+                          className="text-gray-600 cursor-pointer hover:underline text-3xl font-bold"
+                      >{project.projectName}</Link>
+                      <p className="text-sm text-gray-400">
+                          Cliente: {project.clientName}
+                      </p>
+                      <p className="text-sm text-gray-400">
+                          {project.description}
+                      </p>
+                  </div>
+              </div>
+              <div className="flex shrink-0 items-center gap-x-6">
+                  <Menu as="div" className="relative flex-none">
+                      <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                          <span className="sr-only">opciones</span>
+                          <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
+                      </MenuButton>
+                      <Transition as={Fragment} enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95">
+                          <MenuItems
+                              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
+                          >
+                                  <MenuItem>
+                                      <Link to={``}
+                                          className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                                      Ver Proyecto
+                                      </Link>
+                                  </MenuItem>
+                                  <MenuItem>
+                                      <Link to={``}
+                                          className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                                      Editar Proyecto
+                                      </Link>
+                                  </MenuItem>
+                                  <MenuItem>
+                                      <button 
+                                          type='button' 
+                                          className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                          onClick={() => {} }
+                                      >
+                                          Eliminar Proyecto
+                                      </button>
+                                  </MenuItem>
+                          </MenuItems>
+                      </Transition>
+                  </Menu>
+              </div>
+          </li>
+        ))}
+    </ul>
       ) : (
         <p className="text-center py-20">No hay proyectos aún {''}
           <Link
