3d4694af56f14be94ac9faea82356cce0475cc27
Wilson Medina <wilsonmedina8d@gmail.com>
2025-08-04 22:35:09 -0500

    feat: agregar manejo de errores y notificaciones en EditProjectForm

--- taskflow-frontend/src/api/ProjectAPI.ts
+++ taskflow-frontend/src/api/ProjectAPI.ts
@@ -47,7 +47,7 @@ type ProjectAPIType = {
 }
 export async function updateProject({ formData, projectId } : ProjectAPIType ) {
     try {
-        const { data } = await api.put(`/projects/${projectId}`, formData)
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
         return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
--- taskflow-frontend/src/components/projects/EditProjectForm.tsx
+++ taskflow-frontend/src/components/projects/EditProjectForm.tsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import ProjectForm from './ProjectForm'
 import { Project, ProjectFormData } from "@/types/index";
 import { useForm } from 'react-hook-form';
 import { useMutation } from '@tanstack/react-query';
 import { updateProject } from '@/api/ProjectAPI';
+import { toast } from 'react-toastify';
 
 type EditProjectFormProps = {
     data: ProjectFormData;
@@ -12,6 +13,7 @@ type EditProjectFormProps = {
 
 export default function EditProjectForm({ data, projectId } : EditProjectFormProps) {
     
+    const navigate = useNavigate()
     const {register, handleSubmit, formState:{errors}} = useForm({defaultValues: {
         projectName: data.projectName,
         clientName: data.clientName,
@@ -20,11 +22,12 @@ export default function EditProjectForm({ data, projectId } : EditProjectFormPro
 
     const { mutate } = useMutation({
         mutationFn: updateProject,
-        onError:() => {
-
+        onError:(error) => {
+            toast.error(error.message)
         },
-        onSuccess: () => {
-
+        onSuccess: (data) => {
+            toast.success(data)
+            navigate('/')
         }
     })
 
