60d6e6b9891c445bf3d40faaa5c05ce2b71bda29
Wilson Medina <wilsonmedina8d@gmail.com>
2025-08-13 22:22:41 -0500

    feat: add morgan to logs handle on server

--- taskflow-backend/src/server.ts
+++ taskflow-backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from 'morgan'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
 import { corsConfig } from './config/cors'
@@ -11,6 +12,10 @@ connectDB()
 const app = express()
 app.use(cors(corsConfig))
 
+// logs
+app.use(morgan('dev'))
+
+// Read form data
 app.use(express.json())
 
 // Routes
