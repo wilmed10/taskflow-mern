4214fb7c3b7435f7342812dafd88f679e138bae5
Wilson Medina <wilsonmedina8d@gmail.com>
2025-08-13 22:24:21 -0500

    feat: Add React Query Devtools and update @tanstack/react-query dependencies

--- taskflow-frontend/src/main.tsx
+++ taskflow-frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import Router from './router'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router/>
+      <ReactQueryDevtools/>
     </QueryClientProvider>
   </StrictMode>,
 )
