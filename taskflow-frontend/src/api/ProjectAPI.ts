0d1148646454139a6c051b6dcfd3002e72ac4c54
Wilson Medina <wilsonmedina8d@gmail.com>
2025-08-14 22:33:45 -0500

    Feature: Add functionality to delete projects in DashboardView and ProjectAPI

--- taskflow-frontend/src/api/ProjectAPI.ts
+++ taskflow-frontend/src/api/ProjectAPI.ts
@@ -55,3 +55,14 @@ export async function updateProject({ formData, projectId } : ProjectAPIType ) {
         }
     }
 }
+
+export async function deleteProject(id: Project['_id']) {
+    try {
+        const { data } = await api.delete<string>(`/projects/${id}`)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
--- taskflow-frontend/src/views/DashboardView.tsx
+++ taskflow-frontend/src/views/DashboardView.tsx
@@ -1,15 +1,28 @@
 import { Fragment } from 'react'
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { getProjects } from "@/api/ProjectAPI";
-import { useQuery } from "@tanstack/react-query";
+import { deleteProject, getProjects } from "@/api/ProjectAPI";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import { toast } from 'react-toastify';
 
 export default function DashboardView() {
 
   const { data, isLoading } = useQuery({
     queryKey: ['projects'],
     queryFn: getProjects
+  })
+  
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: deleteProject,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+      queryClient.invalidateQueries({queryKey: ['projects']})
+    }
   })
 
   if(isLoading) return 'Cargando...'
@@ -73,7 +86,7 @@ export default function DashboardView() {
                                       <button 
                                           type='button' 
                                           className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                          onClick={() => {} }
+                                          onClick={() => mutate(project._id) }
                                       >
                                           Eliminar Proyecto
                                       </button>
