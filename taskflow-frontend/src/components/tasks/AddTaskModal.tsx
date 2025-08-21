0e22c8f2dcd326d1477c9e6a2828f1c92b7f6f2e
Wilson Medina <wilsonmedina8d@gmail.com>
2025-08-20 22:30:51 -0500

    feat: first details con project view page

---
+++ taskflow-frontend/src/components/tasks/AddTaskModal.tsx
@@ -0,0 +1,53 @@
+import { Fragment } from 'react';
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
+
+export default function AddTaskModal() {
+
+    return (
+        <>
+            <Transition appear show={true} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={() => { }}>
+                    <TransitionChild
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/60" />
+                    </TransitionChild>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <TransitionChild
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                                    <DialogTitle
+                                        as="h3"
+                                        className="font-black text-4xl  my-5"
+                                    >
+                                        Nueva Tarea
+                                    </DialogTitle>
+
+                                    <p className="text-xl font-bold">Llena el formulario y crea  {''}
+                                        <span className="text-fuchsia-600">una tarea</span>
+                                    </p>
+
+                                </DialogPanel>
+                            </TransitionChild>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
+    )
+}
--- taskflow-frontend/src/views/projects/ProjectDetailsView.tsx
+++ taskflow-frontend/src/views/projects/ProjectDetailsView.tsx
@@ -1,6 +1,35 @@
+import { getProjectById } from "@/api/ProjectAPI"
+import AddTaskModal from "@/components/tasks/AddTaskModal"
+import { useQuery } from "@tanstack/react-query"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 
 export default function ProjectDetailsView() {
-  return (
-    <div>ProjectDetailsView</div>
-  )
+    const navigate = useNavigate()
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+    })
+
+    if(isLoading) return 'Cargando...*'
+    if(isError) return <Navigate to='404' />
+    if(data) return (
+      <>
+        <h1 className="text-5xl font-black" >{data.projectName}</h1>
+        <p className="text-2xl font-light text-gray mt-5">{data.description}</p>
+      
+        <nav className="my-5 flex gap-3">
+          <button
+            type="button"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            onClick={ () => navigate('?newTask=true') }
+          >Agregar Tareas</button>
+        </nav>
+      
+        <AddTaskModal/>
+      </>
+    )
 }
